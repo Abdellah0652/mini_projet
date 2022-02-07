4b08dfedba02999d877a1872d393b3e457de35c1
Abdellah Targaoui <abdellah.targaoui@gmail.com>
2022-02-07 18:30:01 +0100

    create login app  html file

--- basic_login/settings.py
+++ basic_login/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'basic_login.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
